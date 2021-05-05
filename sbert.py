b091f1879e471647e65a60fca348519a454e0df3
Nicholas L. S. Silva <nicholas.silva@arapiraca.ufal.br>
2021-05-04 21:23:59 -0300

    Changing server

--- sbert.py
+++ sbert.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
 
 from sentence_transformers import SentenceTransformer
+from sklearn.cluster import KMeans
 import pandas as pd
-data = pd.read_csv("data/produtos.csv")
-print(data.head())
-
+import community
+data = pd.read_csv("data/produtos14.csv")
 
 sentences = data['descp']
 
 # model = SentenceTransformer('neuralmind/bert-large-portuguese-cased')
+# model = SentenceTransformer('stsb-roberta-base', device='cuda')
 model = SentenceTransformer('stsb-roberta-base')
-embeddings = model.encode(sentences, show_progress_bar=True)
+# embeddings = model.encode(sentences, show_progress_bar=True)
 
-for sentence, embedding in zip(sentences, embeddings):
-    print("Sentence:", sentence)
-    print("Embedding:", embedding)
-    print(len(embedding))
-    print("")
+# k = 8273
+# k = 5
+
+# clustering_model = KMeans(n_clusters=k)
+# clustering_model.fit(embeddings)
+# cluster_assignment = clustering_model.labels_
+
+# clustered_sentences = [[] for i in range(k)]
+# for sentence_id, cluster_id in enumerate(cluster_assignment):
+#     clustered_sentences[cluster_id].append(sentences[sentence_id])
+
+# for i, cluster in enumerate(clustered_sentences):
+#     print("Cluster ", i+1)
+#     cluster
+#     print("")
+print(data)
