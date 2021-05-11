b05f02d6645825e14c7606838f8df1f1ac696de7
Nicholas L. S. Silva <nicholas.silva@arapiraca.ufal.br>
2021-05-10 21:18:23 -0300

    Clustering with KMeans

--- sbert.py
+++ sbert.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
 
+from pandas.core.frame import DataFrame
 from sentence_transformers import SentenceTransformer
 from sklearn.cluster import KMeans
 import pandas as pd
-import community
+
 data = pd.read_csv("data/produtos14.csv")
 
 sentences = data['descp']
 
-# model = SentenceTransformer('neuralmind/bert-large-portuguese-cased')
-# model = SentenceTransformer('stsb-roberta-base', device='cuda')
-model = SentenceTransformer('stsb-roberta-base')
-# embeddings = model.encode(sentences, show_progress_bar=True)
+model = SentenceTransformer('neuralmind/bert-large-portuguese-cased')
+# model = SentenceTransformer('stsb-distilbert-base')
 
-# k = 8273
+embeddings = model.encode(sentences, show_progress_bar=True)
+
+k = 8273
 # k = 5
 
-# clustering_model = KMeans(n_clusters=k)
-# clustering_model.fit(embeddings)
-# cluster_assignment = clustering_model.labels_
+clustering_model = KMeans(n_clusters=k)
+clustering_model.fit(embeddings)
+cluster_assignment = clustering_model.labels_
 
-# clustered_sentences = [[] for i in range(k)]
-# for sentence_id, cluster_id in enumerate(cluster_assignment):
-#     clustered_sentences[cluster_id].append(sentences[sentence_id])
+clustered_sentences = [[] for i in range(k)]
+for sentence_id, cluster_id in enumerate(cluster_assignment):
+    clustered_sentences[cluster_id].append(data.loc[[sentence_id]])
 
-# for i, cluster in enumerate(clustered_sentences):
-#     print("Cluster ", i+1)
-#     cluster
-#     print("")
-print(data)
+for i, cluster in enumerate(clustered_sentences):
+    print("Cluster ", i+1)
+    result_conc = pd.concat(cluster)
+    result_conc.to_csv("clusters/cluster" + str(i) + ".csv", index=False)
