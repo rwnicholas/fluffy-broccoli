f9cd8d68e68e9623d2d2872a5648b3fdd21cd5d8
Nicholas L. S. Silva <nicholas.silva@arapiraca.ufal.br>
2021-05-10 21:18:23 -0300

    Clustering with KMeans

---
+++ validateCluster.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import pandas as pd
+import numpy as np
+
+k = 8273
+
+all_acc = 0
+
+for i in range(k):
+    dataTmp = pd.read_csv("clusters/cluster" + str(i) +".csv")
+
+    uniques_gtin, count_gtin = np.unique(dataTmp['gtin'].values, return_counts=True)
+    all_acc += (np.max(count_gtin)/np.sum(count_gtin))
+
+all_acc/=k
+print(all_acc)
