07a7060dc7fb91fe37307ae6aa008db2b2944cb7
Nicholas L. S. Silva <nicholas.silva@arapiraca.ufal.br>
2021-05-11 13:29:42 -0300

    Testing

--- sbert.py
+++ sbert.py
@@ -12,7 +12,11 @@ data['gtin'] = data['gtin'].apply(lambda x: gtf.valida_gtin(str(x)))
 data = data.dropna()
 data = data.reset_index(drop=True)
 
+print("População:", data['gtin'].count())
+
 k = data['gtin'].nunique(dropna=True)
+
+print("Número de Clusters:", k)
 
 sentences = data['descp']
 
