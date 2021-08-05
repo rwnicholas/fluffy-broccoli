e6a5195ab5652c01d0a49f644705a71c1a9091af
Nicholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2021-08-05 10:32:44 -0300

    Checking if path exists and removing clusters (opt-out)

--- cluster.py
+++ cluster.py
@@ -2,7 +2,7 @@ import pandas as pd
 from FKMeans import FaissKMeans
 from sklearn.cluster import KMeans as skKMeans
 import validateCluster as valCluster
-import re
+import os, glob
 
 def preprocessing(text):
     # unitData = ['gr', 'g', 'grama', 'gramas', 'kg', 'kgs', 'quilo', 'quilos', 'quilograma', 'quilogramas', 'kilo', 'kilos', 'kilograma', 'kilogramas', 'm', 'metro', 'metros', 'cm', 'centímetro', 'centimetro', 'l', 'litro', 'litros', 'ml', 'mililitros', 'caixa', 'caixas', 'cx', 'cxs', 'balde', 'baldes', 'sache', 'pacote', 'pacotes', 'pc', 'pct', 'pcs', 'pcts', 'c/', 'com', 'folha', 'folhas', 'fl', 'fls', 'sachê', 'saches', 'sachês', 'envelope', 'envelopes', 'env', 'envs', 'saco', 'sacos', 'un', 'qtd', 'und', 'cxt']
@@ -26,7 +26,7 @@ def cpu_clustering(k_, embeddings_, data_):
 def gpu_clustering(k_, embeddings_, data_):
     __clustering(k_, embeddings_, data_, FaissKMeans)
 
-def __clustering(k, embeddings, data, kmeans_):
+def __clustering(k, embeddings, data, kmeans_, keep_clusters = False):
     clustering_model = kmeans_(n_clusters=k, n_init=1, max_iter=50)
     clustering_model.fit(embeddings)
     cluster_assignment = clustering_model.labels_
@@ -34,6 +34,9 @@ def __clustering(k, embeddings, data, kmeans_):
     clustered_sentences = [[] for i in range(k)]
     for sentence_id, cluster_id in enumerate(cluster_assignment):
         clustered_sentences[cluster_id].append(data.loc[[sentence_id]])
+
+    if not os.path.exists("clusters/"):
+        os.mkdir("clusters/")
 
     countK = 0
     for cluster in clustered_sentences:
@@ -45,3 +48,8 @@ def __clustering(k, embeddings, data, kmeans_):
 
 
     print("Acurácia média:", valCluster.accClusters(countK))
+
+    if keep_clusters == False:
+        files = glob.glob('clusters/*')
+        for f in files:
+            os.remove(f)
