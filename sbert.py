fa50c2b650571afce53ca51699508cd9daa6ca3b
Nicholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2021-10-22 13:31:49 -0300

    Commenting homogeneity test

--- sbert.py
+++ sbert.py
@@ -25,58 +25,60 @@ sentences = data['descp']
 
 model = SentenceTransformer('./paraphrase-distilroberta-base-sefaz', device='cuda')
 
-embeddings = model.encode(sentences, show_progress_bar=True)
-
-# Homogeneity Test
-K = [*range(100, k+(3*10**4), 5000)]
-K.append(k)
-K.remove(80100)
-K.sort()
-
-homogeneity_all = []
-time_all = []
-savingDataList = []
+embeddings = model.encode(sentences, show_progress_bar=True, batch_size=16)
+cluster.gpu_clustering(k, embeddings, data, keep_clusters=False)
 
 
-for k_ in K:
-    start = time()
+# # Homogeneity Test
+# K = [*range(100, k+(3*10**4), 5000)]
+# K.append(k)
+# K.remove(80100)
+# K.sort()
 
-    homogeneity = cluster.gpu_clustering(k_, embeddings, data, keep_clusters=False)
-    homogeneity_all.append(homogeneity)
+# homogeneity_all = []
+# time_all = []
+# savingDataList = []
 
-    end = time()
 
-    run_time = end - start
-    time_all.append(run_time)
+# for k_ in K:
+#     start = time()
 
-    tmpDict = {
-        'K Value': k_,
-        'Homogeneity': homogeneity,
-        'Runtime': run_time
-    }
-    savingDataList.append(tmpDict)
+#     homogeneity = cluster.gpu_clustering(k_, embeddings, data, keep_clusters=False)
+#     homogeneity_all.append(homogeneity)
 
-# Saving results
-savingDataframe = DataFrame(savingDataList)
-savingDataframe.to_csv("data/homogeneity_runtime_results.csv", index=False)
+#     end = time()
 
-# Ploting results
-plt.plot(K, homogeneity_all, 'bx-')
-plt.xlabel('Values of K')
-plt.ylabel('Homogeneity')
-plt.title('The Homogeneity for numbers of clusters')
-plt.xticks(K, rotation=75, fontsize=10)
-plt.margins(x=0)
-plt.tight_layout()
-plt.savefig("homogeneity_graph.png")
-plt.close()
+#     run_time = end - start
+#     time_all.append(run_time)
 
-plt.plot(K, time_all, 'bx-')
-plt.xlabel('Values of K')
-plt.ylabel('Time (seconds)')
-plt.title('Runtime for numbers of clusters')
-plt.xticks(K, rotation=75, fontsize=10)
-plt.margins(x=0)
-plt.tight_layout()
-plt.savefig("runtime_graph.png")
-plt.close()
+#     tmpDict = {
+#         'K Value': k_,
+#         'Homogeneity': homogeneity,
+#         'Runtime': run_time
+#     }
+#     savingDataList.append(tmpDict)
+
+# # Saving results
+# savingDataframe = DataFrame(savingDataList)
+# savingDataframe.to_csv("data/homogeneity_runtime_results.csv", index=False)
+
+# # Ploting results
+# plt.plot(K, homogeneity_all, 'bx-')
+# plt.xlabel('Values of K')
+# plt.ylabel('Homogeneity')
+# plt.title('The Homogeneity for numbers of clusters')
+# plt.xticks(K, rotation=75, fontsize=10)
+# plt.margins(x=0)
+# plt.tight_layout()
+# plt.savefig("homogeneity_graph.png")
+# plt.close()
+
+# plt.plot(K, time_all, 'bx-')
+# plt.xlabel('Values of K')
+# plt.ylabel('Time (seconds)')
+# plt.title('Runtime for numbers of clusters')
+# plt.xticks(K, rotation=75, fontsize=10)
+# plt.margins(x=0)
+# plt.tight_layout()
+# plt.savefig("runtime_graph.png")
+# plt.close()
