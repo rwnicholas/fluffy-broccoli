529cf2665ebfc06a51eceb1384b3dd510bca6cef
Nicholas L. S. Silva <nicholas.silva@arapiraca.ufal.br>
2021-11-18 20:10:03 -0300

    Mining data

--- collect_data_for_training/collect.py
+++ collect_data_for_training/collect.py
@@ -12,23 +12,31 @@ import sqlite3
 from Request import RequestSEFAZ
 request = RequestSEFAZ()
 
-# banco de dados
-database = sqlite3.connect("products.db")
-cursor = database.cursor()
-# criando a tabela de nomes e atributos
-cursor.execute("CREATE TABLE IF NOT EXISTS products ("
-    +"gtin TEXT NOT NULL,"
-    +"descp TEXT NOT NULL PRIMARY KEY,"
-    +"ncm TEXT NOT NULL,"
-    +"preco REAL NOT NULL"
-    +")")
+def run():
+    # banco de dados
+    database = sqlite3.connect("products.db")
+    cursor = database.cursor()
+    # criando a tabela de nomes e atributos
+    cursor.execute("CREATE TABLE IF NOT EXISTS products ("
+        +"gtin TEXT NOT NULL,"
+        +"descp TEXT NOT NULL PRIMARY KEY,"
+        +"ncm TEXT NOT NULL,"
+        +"preco REAL NOT NULL"
+        +")")
 
-products = pd.read_csv("searchingProducts.csv")
-for product in products['descp']:
-    data = request.request(product)
-    for item in data.json():
-        cursor.execute("INSERT OR REPLACE INTO products (gtin, descp, ncm, preco)"
-            +" VALUES (?,?,?,?)",
-            (item['codGetin'],item['dscProduto'],item['codNcm'],item['valUnitarioUltimaVenda']))
-        
-        database.commit()
+    products = pd.read_csv("searchingProducts.csv")
+    for product in products['descp']:
+        data = request.request(product)
+        for item in data.json():
+            if isinstance(item, str):
+                continue
+            elif item['codGetin'] == "SEM GTIN":
+                continue
+            elif item['codGetin'] == None:
+                continue
+
+            cursor.execute("INSERT OR REPLACE INTO products (gtin, descp, ncm, preco)"
+                +" VALUES (?,?,?,?)",
+                (item['codGetin'],item['dscProduto'],item['codNcm'],item['valUnitarioUltimaVenda']))
+            
+            database.commit()
