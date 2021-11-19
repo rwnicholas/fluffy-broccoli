08dbc7d95f3fa7a7927c137d8958bb978c08c1d5
Nicholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2021-11-19 08:59:11 -0300

    Files from presentation

--- sbert.py
+++ sbert.py
@@ -26,60 +26,39 @@ sentences = data['descp']
 model = SentenceTransformer('./paraphrase-distilroberta-base-sefaz', device='cuda')
 
 embeddings = model.encode(sentences, show_progress_bar=True)
-# print(cluster.gpu_clustering(k, embeddings, data, keep_clusters=False))
+print(cluster.gpu_clustering(k, embeddings, data, keep_clusters=False))
+
+# cluster.find_k(k, embeddings)
+
+# # Homogeneity Test
+# K = [*range(100, (2*10**4), 500)]
+# K.append(k)
+# # K.remove(80100)
+# K.sort()
+
+# homogeneity_all = []
+# time_all = []
+# savingDataList = []
 
 
+# for k_ in K:
+#     start = time()
 
-# Adjusted Rand Index
-K = [*range(100, k+(3*10**4), 5000)]
-K.append(k)
-K.remove(80100)
-K.sort()
+#     homogeneity = cluster.gpu_clustering(k_, embeddings, data, keep_clusters=False)
+#     homogeneity_all.append(homogeneity)
 
-adjusted_rand_index_all = []
-time_all = []
-savingDataList = []
+#     end = time()
 
+#     run_time = end - start
+#     time_all.append(run_time)
 
-for k_ in K:
-    start = time()
+#     tmpDict = {
+#         'K Value': k_,
+#         'Homogeneity': homogeneity,
+#         'Runtime': run_time
+#     }
+#     savingDataList.append(tmpDict)
 
-    adjusted_rand_index = cluster.gpu_clustering(k_, embeddings, data, keep_clusters=False)
-    adjusted_rand_index_all.append(adjusted_rand_index)
-
-    end = time()
-
-    run_time = end - start
-    time_all.append(run_time)
-
-    tmpDict = {
-        'K Value': k_,
-        'Adjusted Rand Index': adjusted_rand_index,
-        'Runtime': run_time
-    }
-    savingDataList.append(tmpDict)
-
-# Saving results
-savingDataframe = DataFrame(savingDataList)
-savingDataframe.to_csv("data/adjusted_rand_index_runtime_results.csv", index=False)
-
-# Ploting results
-plt.plot(K, adjusted_rand_index_all, 'bx-')
-plt.xlabel('Values of K')
-plt.ylabel('Adjusted Rand Index')
-plt.title('Adjusted Rand Index for numbers of clusters')
-plt.xticks(K, rotation=75, fontsize=10)
-plt.margins(x=0)
-plt.tight_layout()
-plt.savefig("adjusted_rand_index_graph.png")
-plt.close()
-
-plt.plot(K, time_all, 'bx-')
-plt.xlabel('Values of K')
-plt.ylabel('Time (seconds)')
-plt.title('Runtime for numbers of clusters')
-plt.xticks(K, rotation=75, fontsize=10)
-plt.margins(x=0)
-plt.tight_layout()
-plt.savefig("runtime_graph.png")
-plt.close()
+# # Saving results
+# savingDataframe = DataFrame(savingDataList)
+# savingDataframe.to_csv("data/homogeneity_runtime_results_al.csv", index=False)
